864b9a40f5f2616bfd2254e3e874e1633e9f7f0a
supnep <nguyenquangtrieu99@gmail.com>
2025-04-22 01:32:07 +0700

    x

--- ServiceIntegrationMain/Program.cs
+++ ServiceIntegrationMain/Program.cs
@@ -2,13 +2,14 @@
 
 namespace ServiceIntegrationMain;
 
-internal class Program
+public class Program
 {
     private static int Main(string[] args)
     {
         Console.WriteLine(Greeting.Welcome);
         Console.WriteLine(Greeting.Askforchoice);
-        
+        Console.WriteLine("\x1b[32mThis text is green\x1b[0m");
+
         return 1;
     }
 }
